e86f7d5e0149ade72ec4c5bd53aaba38096e6214
insu <dlstn0201@daum.net>
2025-10-14 17:33:09 +0900

    feat(Calculator): 문자열 입력받는 기능 추가

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+    private String input;
+
+
+    public void userinput() {
+        input = Console.readLine();
+    }
+}
