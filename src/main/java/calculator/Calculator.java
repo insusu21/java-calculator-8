f4966e73cea4b77390cdc83558c93886793d18a4
insu <dlstn0201@daum.net>
2025-10-14 17:37:16 +0900

    feat(Calculator): 리스트로 들어온 숫자를 더해서 더한 결과를 반환하는 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,12 +1,20 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class Calculator {
     private String input;
 
-
     public void userinput() {
         input = Console.readLine();
+    }
+
+    public int add(List<Integer> nums) {
+        int result = 0;
+        for (int a : nums) {
+            result += a;
+        }
+        return result;
     }
 }
