65527e58f85e8f4621a57ad01a76ec0e87546399
insu <dlstn0201@daum.net>
2025-10-15 21:04:06 +0900

    feat(Devider): 커스텀 구분자가 있는지 확인해서 분리하는 기능

---
+++ src/main/java/calculator/Devider.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Devider {
+    String custom = null;
+    String input;
+
+    public Devider(String input) {
+        this.input = input;
+    }
+    public void customdevider(String input) {
+        if (input.startsWith("//")) {
+            Pattern pattern = Pattern.compile("//(.*)\\n(.*)");
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                custom = matcher.group(1);
+                this.input = matcher.group(2);
+            }
+        }
+    }
+
+}
+
