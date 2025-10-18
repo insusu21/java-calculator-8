03cfa765a5319d5d5d00db36d660c0ec9419283c
insu <dlstn0201@daum.net>
2025-10-18 22:46:57 +0900

    refactor: 불필요한 try-catch문 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,13 +5,9 @@ public class Application {
         int result;
         Calculator cal = new Calculator();
         cal.userinput();
-        try {
-            Divider div = new Divider(cal.getinput());
-            div.customdivider();
-            result = cal.add(div.divide());
-            cal.print(result);
-        } catch (IllegalArgumentException e){
-            throw e;
-        }
+        Divider div = new Divider(cal.getinput());
+        div.customdivider();
+        result = cal.add(div.divide());
+        cal.print(result);
     }
 }
